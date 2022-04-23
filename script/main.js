446ac2f1a59ce3b7b020471dc8f70e4e52872ded
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-04-23 09:13:59 -0300

    initial import

---
+++ script/main.js
@@ -0,0 +1,41 @@
+// Carne 400gr por perssoa +6h = 600gr
+// Cerveja 1200ml por pessoa +6h = 2000ml
+// Refrigerante 1000ml por pessoa +6h = 1500ml
+
+// criancas = 0,5 pessoa
+const calcular = document.querySelector('#calcular');
+const resultado = document.querySelector('#resultado');
+const adultos = document.querySelector('#adultos');
+const crianca = document.querySelector('#criancas');
+const tempo = document.querySelector('#tempo');
+calcular.addEventListener('click', comidaNecessaria)
+
+let carne;
+let cerveja;
+let refrigerante;
+
+function comidaNecessaria () {
+    if (tempo.value >= '6') {
+        carne = 600;
+        cerveja = 2000;
+        refrigerante = 1500;
+    } else {
+        carne = 400;
+        cerveja = 1200;
+        refrigerante = 1000;
+    }
+    let criancas = crianca.value * 0.5;
+    let carneCrianca = criancas * carne;
+    let carneAdultos = adultos.value * carne;
+    let carneTotal = (carneCrianca + carneAdultos) / 1000;
+    
+    let cervejaTotal = adultos.value * cerveja / 1000;
+
+    let refriAdultos = adultos.value * refrigerante;
+    let refriCrianca = criancas * refrigerante;
+    let refriTotal = (refriAdultos + refriCrianca) / 1000;
+
+    console.log(`Você vai ter que consumir ${carneTotal}Kg de carne`);
+    console.log(`Você vai ter que comprar ${cervejaTotal}L de cerveja`);
+    console.log(`Você vai ter que comprar ${refriTotal}L de refrigerante`);
+};
